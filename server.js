d8a227fa71e648d19f31c6d6eaf80170ad4d4810
Jackie Robson <jackie.anne.robson@gmail.com>
2019-01-03 12:52:01 +0000

    added compression

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
   var express = require('express');
+  const compression = require('compression');
   var app = express();
   var path = require('path');
   var port = process.env.PORT || 3000;
@@ -7,6 +8,8 @@
       res.sendFile(path.join(__dirname + '/public/index.html'));
   });
   app.use(express.static('public'))
+
+  app.use(compression());
 
   app.listen(port, function() {
       console.log('running');
