74ed06f4db3bb36d18a56faa55a242d6ca94c249
Jackie Robson <jackie.anne.robson@gmail.com>
2019-01-03 13:06:46 +0000

    last fix to server perf

--- server.js
+++ server.js
@@ -4,12 +4,12 @@
   var path = require('path');
   var port = process.env.PORT || 3000;
 
+  app.use(compression());
+
   app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/public/index.html'));
   });
   app.use(express.static('public'))
-
-  app.use(compression());
 
   app.listen(port, function() {
       console.log('running');
