ac0e465e82cf2b186a23f0cc7d14a6fdc52846aa
Jackie Robson <jackie.anne.robson@gmail.com>
2019-01-03 12:23:31 +0000

    added ENV variable for port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
   var express = require('express');
   var app = express();
   var path = require('path');
+  var port = process.env.PORT || 3000;
 
   app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/public/index.html'));
   });
   app.use(express.static('public'))
 
-  app.listen(5000, function() {
+  app.listen(port, function() {
       console.log('running');
   });
